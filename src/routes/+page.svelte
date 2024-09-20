0868b088bd2926ed5ae9f654a1fc908821d38366
vachan-maker <vachan@tutanota.com>
2024-09-20 11:27:20 +0530

    Headings and stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,16 @@
 <script>
 	import Card from "./card.svelte";
 
+  import { Heading } from 'flowbite-svelte';
+
+
     export let data;
     const {nobelPrizes} = data;
     console.log(nobelPrizes);
 </script>
 {#each nobelPrizes as nobelPrize}
-<h1>{nobelPrize.categoryFullName.en}</h1>
-<div class="flex flex-row">
+<Heading tag= "h2">{nobelPrize.categoryFullName.en}</Heading>
+<div class="flex flex-row gap-x-6">
     {#each nobelPrize.laureates as laureate}
 <Card heading="{laureate.fullName.en}" text="{laureate.motivation.en}"/>
 {/each}
